22c17e7f2031202cc2d72c196b63595e811a4a14
root <root@Debian-60-squeeze-64-minimal.(none)>
2013-05-25 20:15:37 +0200

    Updated hospital list code

--- src/services/hospitalList.js
+++ src/services/hospitalList.js
@@ -8,7 +8,7 @@ function setCollections(incomingCollections){
 
 function hospitalList(req, res, next){
 	res.writeHead(200);
-	http.get("http://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=uk_nhs_hospitals&query=select%20name%20from%20%60swdata%60",
+	http.get("http://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=uk_nhs_hospitals&query=select%20name%2C%20lat%2C%20lng%20from%20%60swdata%60",
 		function(getres){
 			getres.pipe(res);	
 		});

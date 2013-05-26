05cb10b69ea6d3a15517499985b4352a591f43b2
root <root@Debian-60-squeeze-64-minimal.(none)>
2013-05-26 13:30:30 +0200

    Added date mods to hospital list

--- src/services/hospitalList.js
+++ src/services/hospitalList.js
@@ -7,10 +7,26 @@ function setCollections(incomingCollections){
 
 
 function hospitalList(req, res, next){
+	var hospitalList = "";
+
 	res.writeHead(200, {'content-type': 'application/json'});
 	http.get("http://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=uk_nhs_hospitals&query=select%20name%2C%20lat%2C%20lng%20from%20%60swdata%60",
 		function(getres){
-			getres.pipe(res);	
+			getres.on("data", function(data){
+				hospitalList += data;
+			})
+			getres.on("end", function(){
+				var hospitalListObject = JSON.parse(hospitalList);
+				var dateTimeStamp = new Date().getTime();
+				for (var i = 0; i < 3; i++) {
+					hospitalListObject[i].dateModified = dateTimeStamp;					
+				};
+				for (var i = 3; i < hospitalListObject.length; i++) {
+					hospitalListObject[i].dateModified = 0;					
+				};
+				res.write(JSON.stringify(hospitalListObject));
+				res.end();
+			})
 		});
 }
 

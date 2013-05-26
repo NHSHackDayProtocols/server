b14af858c7d8252193beff518b93a2cc5c31b1b0
root <root@Debian-60-squeeze-64-minimal.(none)>
2013-05-26 15:23:44 +0200

    updated changehandling

--- src/www/assets/assets/js/changeHandling.js
+++ src/www/assets/assets/js/changeHandling.js
@@ -1,24 +1,16 @@
 function onChangeHandler(event){
 	var address = event.target.id.split("/");
 
-	var multiAddress = findItem(address);
-
-	var node = jsonDocument;
-
-	for (var i = 0; i < multiAddress.arrayAddress.length; i++) {
-		node = node.children[multiAddress.arrayAddress[i]];
-	};
-
-	node[multiAddress.field] = $(event.target).val();
+	setItem(address, $(event.target).val());
 }
 //"0/0/1/3.title"
 function setItem(arrayAddress, value){
 	var node = jsonDocument;
 
-	for (var i = 0; i < multiAddress.arrayAddress.length; i++) {
+	for (var i = 0; i < arrayAddress.length; i++) {
 		if(i == arrayAddress.length - 1){
-			var finalAddress = arrayAddress.split(".")
-			node[finalAddress[0]][finalAddress[1]] = value;
+			var finalAddress = arrayAddress[i].split(".")
+			node.children[finalAddress[0]][finalAddress[1]] = value;
 		}
 		node = node.children[arrayAddress[i]];
 	};
